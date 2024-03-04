8d8718fd4bfc57a1e39ad9f27fcb046c32c4b5f2
Vitaly Zeenko <dev.zeenko@gmail.com>
2024-03-04 19:42:17 +0100

    Validate user's username

--- lib/profile/view/username_text_field.dart
+++ lib/profile/view/username_text_field.dart
@@ -1,3 +1,4 @@
+import 'package:chat_gemini/utils/validators/validators.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,11 @@ class UsernameTextField extends StatelessWidget {
     this.onEdit,
     this.hint,
     this.isLoading = false,
+    this.formKey,
     TextEditingController? controller,
   }) : _controller = controller ?? TextEditingController();
 
+  final Key? formKey;
   final bool isLoading;
   final ValueChanged<String>? onEdit;
   final String? hint;
@@ -20,44 +23,48 @@ class UsernameTextField extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          child: TextField(
-            enabled: !isLoading,
-            controller: _controller,
-            cursorColor: Colors.grey.shade500,
-            decoration: InputDecoration(
-              hintText: hint ?? 'Enter username',
-              contentPadding: const EdgeInsets.all(12),
-              labelStyle: Theme.of(context).textTheme.labelSmall,
-              hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context)
-                        .textTheme
-                        .labelMedium!
-                        .color!
-                        .withOpacity(0.5),
-                  ),
-              suffixIcon: isLoading
-                  ? const CupertinoActivityIndicator()
-                  : IconButton(
-                      onPressed: () {
-                        if (isLoading) return;
-                        final text = _controller.text.trim();
-
-                        if (text.isNotEmpty) {
-                          onEdit?.call(text);
-                          _controller.clear();
-                        }
-                      },
-                      icon: const Icon(Icons.edit),
+          child: Form(
+            key: formKey,
+            child: TextFormField(
+              enabled: !isLoading,
+              controller: _controller,
+              cursorColor: Colors.grey.shade500,
+              validator: usernameValidation,
+              decoration: InputDecoration(
+                hintText: hint ?? 'Enter username',
+                contentPadding: const EdgeInsets.all(12),
+                labelStyle: Theme.of(context).textTheme.labelSmall,
+                hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context)
+                          .textTheme
+                          .labelMedium!
+                          .color!
+                          .withOpacity(0.5),
                     ),
-            ),
-            onSubmitted: (text) {
-              if (isLoading) return;
+                suffixIcon: isLoading
+                    ? const CupertinoActivityIndicator()
+                    : IconButton(
+                        onPressed: () {
+                          if (isLoading) return;
+                          final text = _controller.text.trim();
 
-              if (text.trim().isNotEmpty) {
-                onEdit?.call(text);
-                _controller.clear();
-              }
-            },
+                          if (text.isNotEmpty) {
+                            onEdit?.call(text);
+                            _controller.clear();
+                          }
+                        },
+                        icon: const Icon(Icons.edit),
+                      ),
+              ),
+              onFieldSubmitted: (text) {
+                if (isLoading) return;
+
+                if (text.trim().isNotEmpty) {
+                  onEdit?.call(text);
+                  _controller.clear();
+                }
+              },
+            ),
           ),
         ),
       ],
