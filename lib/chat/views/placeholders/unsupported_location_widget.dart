3df40d701202bb5bf7ed20aaa0baa88fce031087
Vitaly Zeenko <dev.zeenko@gmail.com>
2024-03-29 15:41:27 +0100

    Add Reload button to Unsupported location screen

--- lib/chat/views/placeholders/unsupported_location_widget.dart
+++ lib/chat/views/placeholders/unsupported_location_widget.dart
@@ -3,32 +3,55 @@ import 'package:gap/gap.dart';
 
 class UnsupportedLocationWidget extends StatelessWidget {
   const UnsupportedLocationWidget({
+    this.onReload,
     super.key,
   });
+
+  final VoidCallback? onReload;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(24),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.asset(
-            'assets/images/map_pointer_bg.png',
-            height: 164,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/map_pointer_bg.png',
+                  height: 164,
+                ),
+                const Gap(20),
+                Text(
+                  'Unsupported location',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const Gap(20),
+                Text(
+                  'We do not support this location yet.',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
-          const Gap(20),
-          Text(
-            'Unsupported location',
-            style: Theme.of(context).textTheme.headlineLarge,
-            textAlign: TextAlign.center,
-          ),
-          const Gap(20),
-          Text(
-            'We do not support this location yet.',
-            style: Theme.of(context).textTheme.headlineSmall,
-            textAlign: TextAlign.center,
+          Flexible(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: onReload,
+                    child: const Text('Reload'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
